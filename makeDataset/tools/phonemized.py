d1db04d9fe385c62cc2cd6e9f0c9fb5b60bd8605
ElmorenohWTF <62709646+ElmorenohWTF@users.noreply.github.com>
2023-12-30 23:18:03 +0100

    Update phonemized.py

--- makeDataset/tools/phonemized.py
+++ makeDataset/tools/phonemized.py
@@ -1,14 +1,14 @@
 from phonemizer import phonemize
 import os
 
-with open('/home/eleven/StyleGuide/makeDataset/trainingdata/output.txt', 'r') as f: # Path to output.txt
+with open('/content/CreateVoiceDatasetsOnColab/makeDataset/tools/output.txt', 'r') as f: # Path to output.txt
     lines = f.readlines()
 
 # Phonemize the transcriptions
 phonemized_lines = []
 for line in lines:
     filename, transcription, speaker = line.strip().split('|')
-    phonemes = phonemize(transcription, language='en-us', backend='espeak')
+    phonemes = phonemize(transcription, language='es', backend='espeak')
     phonemized_lines.append((filename, f'{filename}|{phonemes}|{speaker}\n'))
 
 # Sort
@@ -18,10 +18,10 @@ phonemized_lines.sort(key=lambda x: int(x[0].split('_')[1].split('.')[0]))
 train_lines = phonemized_lines[:int(len(phonemized_lines) * 0.9)]
 val_lines = phonemized_lines[int(len(phonemized_lines) * 0.9):]
 
-with open('/home/eleven/StyleGuide/makeDataset/trainingdata/train_list.txt', 'w') as f: # Path for train_list.txt in the training data folder
+with open('/content/CreateVoiceDatasetsOnColab/makeDataset/tools/train_list.txt', 'w') as f: # Path for train_list.txt in the training data folder
     for _, line in train_lines:
         f.write(line)
 
-with open('/home/eleven/StyleGuide/makeDataset/trainingdata/val_list.txt', 'w') as f:  # Path for val_list.txt in the training data folder
+with open('/content/CreateVoiceDatasetsOnColab/makeDataset/tools/val_list.txt', 'w') as f:  # Path for val_list.txt in the training data folder
     for _, line in val_lines:
         f.write(line)
