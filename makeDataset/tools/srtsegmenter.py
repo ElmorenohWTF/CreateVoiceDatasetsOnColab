5817a2a1e69acd3b96f8a59ce16e76fc9dbf33d1
ElmorenohWTF <62709646+ElmorenohWTF@users.noreply.github.com>
2023-12-30 23:03:16 +0100

    Update srtsegmenter.py

--- makeDataset/tools/srtsegmenter.py
+++ makeDataset/tools/srtsegmenter.py
@@ -36,5 +36,5 @@ with open('/content/CreateVoiceDatasetsOnColab/makeDataset/tools/output.txt', 'w
             audio_segment.export(os.path.join(bad_audio_dir, filename), format='wav')
 
 
-        phonemized_text = phonemize(sub.text, language='es-es')
+        phonemized_text = phonemize(sub.text, language='es')
         phonemized_file.write(f'{i}\n{str(sub.start)} --> {str(sub.end)}\n{phonemized_text}\n\n')
